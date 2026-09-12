c0fd3d293560e97aaeb750a042ed2442a1ac0046
Sharlot <sharlot_adrian@ucne.edu.do>
2026-09-12 10:34:42 -0400

    Creacion del modelo libro.

---
+++ RegistroLibros/Models/Libro.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistroLibros.Models
+{
+    public class Libro
+    {
+        public int LibroId { get; set; }
+        public string Titulo { get; set; }
+
+        public string autor {  get; set; }
+
+        public DateTime AnoPublicacion { get; set; }
+    }
+}
